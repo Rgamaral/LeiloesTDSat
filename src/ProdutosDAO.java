9121e4d0c4c5d8cfd8401103f9b7b8d414d862ad
Rgamaral <professorrgamaral@gmail.com>
2025-05-24 20:30:55 -0300

    Primeiro commit: estrutura inicial do projeto

---
+++ src/ProdutosDAO.java
@@ -0,0 +1,56 @@
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+
+public class ProdutosDAO {
+    
+    Connection conn;
+    PreparedStatement prep;
+    ResultSet resultset;
+    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+    
+    public void cadastrarProduto (ProdutosDTO produto){
+        conn = new conectaDAO().connectDB(); // Remover o comentário
+    
+    try {
+        String sql = "INSERT INTO produtos(nome, valor, status) VALUES(?,?,?)";
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+        prep.executeUpdate();
+    } catch (SQLException e) {
+        JOptionPane.showMessageDialog(null, "Erro ao cadastrar: " + e.getMessage());
+    }
+    }
+    
+    public ArrayList<ProdutosDTO> listarProdutos(){
+        conn = new conectaDAO().connectDB();
+    
+    try {
+        String sql = "SELECT * FROM produtos";
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+        
+        while(resultset.next()) {
+            ProdutosDTO produto = new ProdutosDTO();
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+            listagem.add(produto);
+        }
+    } catch (SQLException e) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar: " + e.getMessage());
+    }
+    return listagem;
+    }
+    
+    
+    
+        
+}
+
